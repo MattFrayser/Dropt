2e8a9931d2f0cb5966536530d27757cd237264e0
mattfrayser <mattfrayser@gmail.com>
2025-12-04 15:36:17 -0500

    Create top level file for AppError since it is used globally

---
+++ src/errors.rs
@@ -0,0 +1,30 @@
+// AppError is used throughout server
+// return 500 to client side, log errors serverside
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use tracing::error;
+
+/// Automatic conversion: any error -> HTTP 500 response
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        error!(
+            error = ?self.0,
+            backtrace = ?self.0.backtrace(),
+            "Internal Server error"
+        );
+        StatusCode::INTERNAL_SERVER_ERROR.into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error> + Send + Sync,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
